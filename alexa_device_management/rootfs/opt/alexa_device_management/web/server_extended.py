ed6b86fd1898312e221aed265fc58f32d590460f
Supermario1907 <32839601+nikodemusz@users.noreply.github.com>
2026-07-16 17:31:48 +0200

    Load non-blocking HA export editor handlers

--- alexa_device_management/rootfs/opt/alexa_device_management/web/server_extended.py
+++ alexa_device_management/rootfs/opt/alexa_device_management/web/server_extended.py
@@ -12,7 +12,7 @@ import ha_export
 import ha_export_overrides
 import server_clean
 
-APP_VERSION = "2.8.0-rc1"
+APP_VERSION = "2.8.1-rc1"
 
 
 @web.middleware
@@ -44,6 +44,7 @@ async def navigation_middleware(request: web.Request, handler):
             + '"></script><script src="' + ingress_path + '/static/ha_export_discovery.js?v=' + APP_VERSION
             + '"></script><script src="' + ingress_path + '/static/ha_export_consistency.js?v=' + APP_VERSION
             + '"></script><script src="' + ingress_path + '/static/ha_export_lifecycle.js?v=' + APP_VERSION
+            + '"></script><script src="' + ingress_path + '/static/ha_export_performance.js?v=' + APP_VERSION
             + '"></script>'
         )
         text = text.replace("let inventory=", "window.inventory=")
